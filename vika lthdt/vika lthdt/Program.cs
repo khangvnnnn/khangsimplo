c7aa34460297eaf9726c03ef4727bc0e3567939f
khangvnnnn <ditttmecuocdoiii@gmail.com>
2025-10-28 10:40:09 +0700

    Add files via upload

---
+++ vika lthdt/vika lthdt/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vika_lthdt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.Write("nhap so thu nhat : ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("nhap so thu hai : ");
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine(
+                "tong la : " + (a + b).ToString());
+            Console.WriteLine(
+                "hieu la : " + (a - b).ToString());
+            Console.WriteLine(
+                "tich la : " + (a * b).ToString());
+            Console.WriteLine(
+                "thuong la : " + (a / b).ToString());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ vika lthdt/vika lthdt/btdecimal.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vika_lthdt
+{
+    class btdecimal
+    {
+        static void Main(string[] args)
+        {
+            decimal a;
+            decimal b;
+            Console.Write("Nhap so thap phan : ");
+            a = decimal.Parse(Console.ReadLine());
+            Console.Write("Nhap so thap phan thu hai : ");
+            b = decimal.Parse(Console.ReadLine());
+            Console.WriteLine("so ban vua nhap la : " + a + " va " + b);
+            Console.WriteLine("Tong = " + decimal.Add(a, b).ToString());
+            Console.WriteLine("Hieu = " + decimal.Subtract(a, b).ToString());
+            Console.WriteLine("Tich = " + decimal.Multiply(a, b).ToString());
+            Console.WriteLine("Thuong = " + decimal.Divide(a, b).ToString());
+
+            Console.WriteLine("Lam tron nguyen: " + decimal.Round(a));
+            Console.WriteLine("Lam tron nguyen: " + decimal.Round(b));
+
+            Console.WriteLine("Lam can tren: " + decimal.Ceiling(a));
+            Console.WriteLine("Lam can tren: " + decimal.Ceiling(b));
+
+            Console.WriteLine("Lam can duoi: " + decimal.Floor(a));
+            Console.WriteLine("Lam can duoi: " + decimal.Floor(b));
+
+            Console.ReadLine();
+
+        }
+    }
+}
